64e7ea185221ef50cf415667db2e5193078aa223
Alexey Arefjev <alexeychallenger@gmail.com>
2018-01-31 22:11:53 +0200

    Change camera script rotation parameters

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using Assets.Scripts.Classes;
+using Assets.Scripts.Utils;
 using System.Collections;
 
 /// <summary>
@@ -7,19 +7,31 @@ using System.Collections;
 /// </summary>
 public class CameraScript : MonoBehaviour
 {
+    /// <summary>
+    /// Target on which the camera is looking
+    /// </summary>
     [Header("Refs")]
-
-    public Transform target;                            //целевой объект на которую смотрит камера
-    public Transform targetPointer;                     //указатель положения цели
-    public Transform cameraPointer;                     //указатель положения камеры
-    public Transform cameraContainer;                   //контейнер с камерой и указателями ее положения
-    public Camera gameCamera;                           //камера
+    public Transform target;
+    /// <summary>
+    /// Target position pointer
+    /// </summary>
+    public Transform targetPointer;
+    /// <summary>
+    /// Camera pointer
+    /// </summary>
+    public Transform cameraPointer;
+    /// <summary>
+    /// Camera container
+    /// </summary>
+    public Transform cameraContainer;
+    public Camera camera;
 
     [Header("Stats")]
 
     public float followSpeed = 3f;                      //скорость следования камеры за целью
     public float zoomSpeed = 2f;                        //скорость зума
-    public float rotateSpeed = 3f;                      //скорость поворота
+    public float rotateSpeedX;
+    public float rotateSpeedY = 3f;                      //скорость поворота
     public Vector3 offset = new Vector3(0, 0, -5);      //положение камеры относительно якоря
     public float vectorTolerance = 0.01f;               //точность округление координат
     public float quaterTolerance = 0.0001f;             //точность округление осей
@@ -29,23 +41,18 @@ public class CameraScript : MonoBehaviour
     public float minZoom = 1f;                          //минимальная дальность зума
     public float maxZoom = 30f;                         //максимальная дальность зума
 
-    private float rotateSpeedX;                         //скорость поворота по оси Х
-
     void Start()
     {
-        rotateSpeedX = rotateSpeed;
         cameraPointer.localPosition = offset;
-        gameCamera.GetComponent<Transform>().localPosition = offset;   //присвоение смещение камеры относительно центра объекта
+        camera.transform.localPosition = offset;   //присвоение смещение камеры относительно центра объекта
     }
 
     void FixedUpdate()
     {
-
         if (Input.GetMouseButtonDown(2))                //при нажатии средней кнопки мыши определяется объект на который указывает курсор
         {
             SelectPivot();
         }
-
         if (Input.GetMouseButton(1))                    //при зажатии правой кнопки мыши разблокируется вращение камеры
         {
             CameraRotate();
@@ -61,21 +68,18 @@ public class CameraScript : MonoBehaviour
                                                   targetPointer.rotation.eulerAngles.y,
                                                   0.0f);
 
-        cameraContainer.rotation = Quaternion.Euler(gameCamera.transform.rotation.eulerAngles.x,
-                                                    gameCamera.transform.rotation.eulerAngles.y,
+        cameraContainer.rotation = Quaternion.Euler(camera.transform.rotation.eulerAngles.x,
+                                                    camera.transform.rotation.eulerAngles.y,
                                                     0.0f);    
         FollowTarget();
 
     }
 
-
-
     /// <summary>
-    /// функция следования камеры за объектом
+    /// Move camera behind the object
     /// </summary>
     private void FollowTarget()
     {
-
         if (target != null)
         {
             if (target.tag != "terrain")
@@ -85,39 +89,39 @@ public class CameraScript : MonoBehaviour
         }
 
         //сглаживание перемещения указателя на объект
-        if (Utils.Vector3Round(targetPointer.localPosition, vectorTolerance) !=
-            Utils.Vector3Round(cameraContainer.localPosition, vectorTolerance))
+        if (RoundUtils.Vector3Round(targetPointer.localPosition, vectorTolerance) !=
+            RoundUtils.Vector3Round(cameraContainer.localPosition, vectorTolerance))
         {
             cameraContainer.localPosition = Vector3.Lerp(cameraContainer.localPosition, targetPointer.localPosition, followSpeed * Time.deltaTime);
         }
 
         //сглаживание зума камеры
-        if (Utils.Vector3Round(gameCamera.transform.position, vectorTolerance) !=
-            Utils.Vector3Round(cameraPointer.position, vectorTolerance))
+        if (RoundUtils.Vector3Round(camera.transform.position, vectorTolerance) !=
+            RoundUtils.Vector3Round(cameraPointer.position, vectorTolerance))
         {
-            gameCamera.transform.position = Vector3.Lerp(gameCamera.transform.position, cameraPointer.position, followSpeed * Time.deltaTime);
+            camera.transform.position = Vector3.Lerp(camera.transform.position, cameraPointer.position, followSpeed * Time.deltaTime);
         }
 
         //сглаживание поворотов камеры вокруг цели
-        if (Utils.Vector3Round(cameraContainer.eulerAngles, quaterTolerance) != Utils.Vector3Round(targetPointer.eulerAngles, quaterTolerance))
+        if (RoundUtils.Vector3Round(cameraContainer.eulerAngles, quaterTolerance) != RoundUtils.Vector3Round(targetPointer.eulerAngles, quaterTolerance))
         {
             cameraContainer.localRotation = Quaternion.Lerp(cameraContainer.localRotation, targetPointer.localRotation, followSpeed * Time.deltaTime);
         }
-
-
     }
 
     /// <summary>
-    /// функция определения положения якоря камеры
+    /// Select pivot point for camera
     /// </summary>
     private void SelectPivot()
     {
         RaycastHit hit;
-        Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);                         //создается луч который определяет объект на который указывает курсор мыши
-        if (Physics.Raycast(ray, out hit))                                                  //определяет место соприкосновения коллайдера луча и объекта
+        //создается луч который определяет объект на который указывает курсор мыши
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        //определяет место соприкосновения коллайдера луча и объекта
+        if (Physics.Raycast(ray, out hit))
         {
-
-            target = hit.transform;                                                         //определение объекта, на который указывает мышь и назначение его целью
+            //определение объекта, на который указывает мышь и назначение его целью
+            target = hit.transform;
             if (target.name == "Terrain")
             {
                 targetPointer.position = hit.point;
@@ -131,62 +135,54 @@ public class CameraScript : MonoBehaviour
         }
         else
         {
-            target = null;                                                                       //сброс цели в случае неудачи
-            //Debug.Log("No target");
+            target = null;
+            Debug.Log("No target");
         }
     }
 
     /// <summary>
-    /// Вращение указателя положения камеры
+    /// Rotating the camera position pointer
     /// </summary>
-    private void CameraRotate()                                                                 
+    private void CameraRotate()
     {
-
-        rotateSpeedX = RotateSpeedConvert(targetPointer.eulerAngles.x); 
+        float currentRotateSpeedX = ConvertXAngleToYAxisRotateSpeed(targetPointer.eulerAngles.x); 
 
         if (Input.GetAxis("Mouse Y") < 0)
         {
-            targetPointer.Rotate(rotateSpeed, 0, 0);
+            targetPointer.Rotate(rotateSpeedY, 0, 0);
         }
         if (Input.GetAxis("Mouse Y") > 0)
         {
-            targetPointer.Rotate(-rotateSpeed, 0, 0);
+            targetPointer.Rotate(-rotateSpeedY, 0, 0);
         }
         if (Input.GetAxis("Mouse X") < 0)
         {
-            targetPointer.Rotate(0, -rotateSpeedX, 0);
+            targetPointer.Rotate(0, -currentRotateSpeedX, 0);
         }
         if (Input.GetAxis("Mouse X") > 0)
         {
-            targetPointer.Rotate(0, rotateSpeedX, 0);
+            targetPointer.Rotate(0, currentRotateSpeedX, 0);
         }
-
         AngleClamp();
-
     }
 
-    
+
     /// <summary>
-    /// Узкоспециализированная функция конвертации угла наклона камеры на оси X в скорость поворота камеры вокруг оси Y на данном угле наклона
+    /// Converting the angle of inclination of the camera on the X axis into the rotation speed of the camera around the Y axis at a given angle of inclination
     /// </summary>
     /// <param name="x"></param>
     /// <returns></returns>
-    public float RotateSpeedConvert (float x)
+    public float ConvertXAngleToYAxisRotateSpeed (float x)
     {
         float res;
-
-        if ((x > 270) && (x < 360))
+        if (x > 270 && x < 360)
         {
             x = 360 - x;
         }
-
         x = 90 - x;
-        res = (float) (x / 0.9 * 0.7);
-
+        res = (x * 0.78f);
         res = Mathf.Sqrt(2 * Mathf.Pow(res, 2));
-
-        res = (float) (res / 0.99 * 0.01*rotateSpeed);
-
+        res = (res * rotateSpeedX);
         return res;
     }
 
@@ -198,21 +194,23 @@ public class CameraScript : MonoBehaviour
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             cameraPointer.localPosition += new Vector3(0, 0, zoomSpeed);
-            cameraPointer.localPosition = new Vector3(
-                                                    cameraPointer.localPosition.x,
-                                                    cameraPointer.localPosition.y,
-                                                    Mathf.Clamp(cameraPointer.localPosition.z, -maxZoom, -minZoom)
-                                                );
+            cameraPointer.localPosition = new Vector3
+            (
+                cameraPointer.localPosition.x,
+                cameraPointer.localPosition.y,
+                Mathf.Clamp(cameraPointer.localPosition.z, -maxZoom, -minZoom)
+            );
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             cameraPointer.localPosition -= new Vector3(0, 0, zoomSpeed);
-            cameraPointer.localPosition = new Vector3(
-                                                    cameraPointer.localPosition.x,
-                                                    cameraPointer.localPosition.y,
-                                                    Mathf.Clamp(cameraPointer.localPosition.z, -maxZoom, -minZoom)
-                                                );
+            cameraPointer.localPosition = new Vector3
+            (
+                cameraPointer.localPosition.x,
+                cameraPointer.localPosition.y,
+                Mathf.Clamp(cameraPointer.localPosition.z, -maxZoom, -minZoom)
+            );
         }
     }
 
@@ -223,19 +221,21 @@ public class CameraScript : MonoBehaviour
     {
         if ((targetPointer.eulerAngles.x > maxXAngle) && (targetPointer.eulerAngles.x <= 90))
         {
-            targetPointer.eulerAngles = new Vector3(
-                                                        maxXAngle,
-                                                        targetPointer.eulerAngles.y,
-                                                        targetPointer.eulerAngles.z
-                                                    );
+            targetPointer.eulerAngles = new Vector3
+            (
+                maxXAngle,
+                targetPointer.eulerAngles.y,
+                targetPointer.eulerAngles.z
+            );
         }
         else if ((targetPointer.eulerAngles.x > 270) && (targetPointer.eulerAngles.x <= 360 - maxXAngle))
         {
-            targetPointer.eulerAngles = new Vector3(
-                                                       -maxXAngle,
-                                                        targetPointer.eulerAngles.y,
-                                                        targetPointer.eulerAngles.z
-                                                    );
+            targetPointer.eulerAngles = new Vector3
+            (
+                -maxXAngle,
+                targetPointer.eulerAngles.y,
+                targetPointer.eulerAngles.z
+            );
         }
     }
 
